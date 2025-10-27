2cf31ae577bc7dfb4054e9ce5e865e48d8960907
tahirainam <cattblackwhite@gmail.com>
2025-10-27 12:15:36 +0530

    modified navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,11 @@ function NavBar() {
       <div className="text-lg font-semibold">Little Lemon</div>
       <ul className="flex space-x-4">
         <li><a href="#home">Home</a></li>
-        <li><a href="#specials">Specials</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#chicago">Chicago</a></li>
-        <li><a href="#booking">Booking</a></li>
+        <li><a href="#specials">About</a></li>
+        <li><a href="#testimonials">Menu</a></li>
+        <li><a href="#chicago">Reservations</a></li>
+        <li><a href="#booking">Order Online</a></li>
+        <li><a href="#booking">Login</a></li>
       </ul>
     </nav>
   );
