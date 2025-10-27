c97bbcba0b7bf56ddf76cbb931ee002e4d5aee78
tahirainam <cattblackwhite@gmail.com>
2025-10-27 12:19:32 +0530

    home page content

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,9 +2,12 @@
 function HomePage() {
   return (
     <div>
+      <h1>Little Lemon</h1>
+      <p>Chicago</p>
       <p>
-        Home Page
+        A brief description of what type of food it offers Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro sint dicta laborum ut molestias consequuntur deserunt eligendi maiores cum possimus, earum amet odio accusantium esse eaque quaerat nemo eum!
       </p>
+      <button>Book a Table</button>
     </div>
   );
 }
