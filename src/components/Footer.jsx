d1ff26264f7bddaf8a9dd91181e5645d4635a99c
tahirainam <cattblackwhite@gmail.com>
2025-10-27 12:40:22 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return(
+        <div>
+            <footer className="footer">
+                <img src="image_url" alt="Little Lemon logo" />
+                {/* doormat links */}
+                {/* contact info */}
+                {/* social media links */}
+                <p>© 2025 Little Lemon. All rights reserved.</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
