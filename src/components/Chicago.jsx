9c26e793cdb5a6a083149ff8271d6515982b2603
tahirainam <cattblackwhite@gmail.com>
2025-10-27 12:35:19 +0530

    added about basic structure

--- src/components/Chicago.jsx
+++ src/components/Chicago.jsx
@@ -1,7 +1,12 @@
 // the component that describes the Little Lemon Chicago restaurant and gives a short backstory for it
 function Chicago() {
-    return (
-      <p>Chicago component</p>  
+  return (
+    <div>
+      <h2>Little Lemon</h2>
+      <p>Chicago</p>
+      <p>Chicago component Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus maiores repellendus inventore, accusamus distinctio vel eligendi ad nisi quidem quis quisquam itaque error doloribus, esse dicta, illo neque! Facere, amet?</p>  
+      <img src="image_url" alt="Little Lemon Chicago owners" />
+    </div>
     );
 }
 
