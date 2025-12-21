ec700db832702799b76dc13419103ccfd00dd819
tahirainam <cattblackwhite@gmail.com>
2025-12-21 01:16:53 -0800

    updated footer dormate navigation links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Footer.css';
 import logo from '../assets/small_logo.png';
+import {Link} from "react-router-dom";
 
 function Footer() {
   return (
@@ -14,10 +15,12 @@ function Footer() {
       <div className="footer-column">
         <h4>Doormat Navigation</h4>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#menu">Menu</a></li>
-          <li><a href="#reservations">Reservations</a></li>
+          <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/reservations">Reservations</Link></li>
+        <li><Link to="/order">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
         </ul>
       </div>
 
